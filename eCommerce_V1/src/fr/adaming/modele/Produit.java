91b13f2986e869d6198de6f89137bdbd66e35939
emegu <emegu@192.168.1.173>
2018-01-08 10:50:02 +0100

    emeric: first commit

---
+++ eCommerce_V1/src/fr/adaming/modele/Produit.java
@@ -0,0 +1,13 @@
+package fr.adaming.modele;
+
+public class Produit {
+	
+	private Long idProduit;
+	private String description;
+	private String designation;
+	private double prix;
+	private int quantite;
+	private boolean selectionne;
+	private String photo;
+
+}
