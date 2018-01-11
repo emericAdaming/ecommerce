08cc8a1dd61ddc3dc03f0c29268319f926f23986
emegu <emegu@192.168.1.173>
2018-01-11 11:29:32 +0100

    emeric commit 11h30

--- eCommerce_V1/src/fr/adaming/managedBeans/ClientManagedBean.java
+++ eCommerce_V1/src/fr/adaming/managedBeans/ClientManagedBean.java
@@ -1,9 +1,13 @@
 package fr.adaming.managedBeans;
 
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.Properties;
 
+import javax.activation.DataHandler;
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
 import javax.faces.bean.ManagedBean;
@@ -14,7 +18,18 @@ import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.mail.util.ByteArrayDataSource;
+import javax.swing.ImageIcon;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.PdfWriter;
 
 import fr.adaming.modele.Client;
 import fr.adaming.service.IClientService;
@@ -69,6 +84,11 @@ public class ClientManagedBean implements Serializable {
 	}
 	
 	public String sendMail(){
+		
+		
+		try {
+		System.out.println("!!!!!!!!!!!!!Creation pdf!!!!!!!!!!!!!!!!!!!!");
+			creationPDF();
 		System.out.println("!!!!!!!!!!!!!!!!!!! ENVOIE MAIL !!!!!!!!!!!!!!!!!!!!!!!!");
 		// Creation protocole
 	    Properties props = new Properties();
@@ -89,11 +109,21 @@ public class ClientManagedBean implements Serializable {
 			System.out.println("!!!!!!!! Message session cree !!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 			message.setFrom(new InternetAddress("ecommerce44000@gmail.com"));
 			message.setRecipients(Message.RecipientType.TO,
-					InternetAddress.parse("alexandre.cocagne@gmail.com"));
+					InternetAddress.parse("eme.guibert49@gmail.com"));
 			System.out.println("!!!!!!!! Destinataire et envoteur done !!!! !!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-			message.setSubject("Testing Subject");
-			message.setText("Dear Mail Crawler," +
-					"\n\n No spam to my email, please!");
+			message.setSubject("Sujet du mail");
+			message.setText("Salut  !!!!!," +
+					"\n\n Regarde le pdf !!"+
+					"\n\n Cordialment! \n l'equipe ecommerce");
+			System.out.println("!!!!!!!!!!!!!! Piece jointe !!!!!!!!!!!!!!!!!!!!!!!!!!");
+			MimeMultipart mp = new MimeMultipart();
+			MimeBodyPart mbp1 = new MimeBodyPart();
+				mbp1.attachFile(new File("C:/Users/emegu/Documents/Test4.pdf"));
+				/*ByteArrayDataSource ds = new ByteArrayDataSource(); 
+				mbp1.setDataHandler(new DataHandler(ds));
+				mbp1.setFileName("Test.pdf");*/
+		    mp.addBodyPart(mbp1);
+			message.setContent(mp);
 			System.out.println("!!!!!!!! Debut transport !!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 			Transport.send(message);
 
@@ -103,7 +133,37 @@ public class ClientManagedBean implements Serializable {
 			throw new RuntimeException(e);
 		}
 		
-		return "login";
+		
+		} catch (DocumentException | IOException e) {
+			e.printStackTrace();
+		}
+		
+		return "accueil";
+	}
+	
+
+	
+	public void creationPDF() throws DocumentException, IOException {
+				String chemin= "C:/Users/emegu/Documents/Test4.pdf"; 
+			 // step 1
+		    	// Using a custom page size
+		        Rectangle pagesize = new Rectangle(216f, 720f);
+		        Document document = new Document();
+		        // step 2
+		        PdfWriter.getInstance(document, new FileOutputStream(chemin));
+		        // step 3
+		        document.open();
+		        Image icon = Image.getInstance("C:/Users/emegu/Pictures/chauve.jpg");
+		        // step 4
+		       // document.addCreationDate();
+		        document.add(new Paragraph(
+		            "\t Hello People! \n" +
+		            "vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv!"));
+		        document.add(icon);
+		        // step 5
+		        document.close();
+		
+		
 	}
 	
 }
